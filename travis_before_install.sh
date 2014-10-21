3eba5d670b83a6c456b75178479ea0b11b99f0ff
cdracars <cdracars@gmail.com>
2014-10-21 11:18:12 -0500

    fixes

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -v
  
 # Fetch the CI GitHub user's encrypted private key
-curl -L -o travisci_rsa.enc "$TRAVIS-KEY-URL"
+curl -L -o travisci_rsa.enc "$URL"
  
 # Decrypt the CI GitHub user's private key and add it to SSH key list
-openssl aes-256-cbc -k "$TRAVIS-KEY-SECRET" -in travisci_rsa.enc -d -a -out id_rsa
+openssl aes-256-cbc -k "$SECRET" -in travisci_rsa.enc -d -a -out id_rsa
 ssh-add -D
 chmod 600 id_rsa
 ssh-add ./id_rsa
