9486c5747eb33786d53fb7ddc08af02c116678b6
Cody Dracars <cdracars@gmail.com>
2014-10-21 15:19:29 -0500

    Update travis_before_install.sh

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -4,11 +4,11 @@
 curl -L -o travisci_rsa.enc "$URL"
  
 # Decrypt the CI GitHub user's private key and add it to SSH key list
-openssl aes-256-cbc -k "$SECRET" -in travisci_rsa.enc -d -a -out id_rsa
+openssl aes-256-cbc -k "$SECRET" -in travisci_rsa.enc -d -a -out ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add -D
 chmod 600 id_rsa
-ssh-add ./id_rsa
+ssh-add ~/.ssh/id_rsa
 ls ~/.ssh 
 
 # Init all submodules now that CI GitHub user's private key is added
