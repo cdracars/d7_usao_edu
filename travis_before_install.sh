30e02465b9c635584b031cf94dd32a43809f79b2
cdracars <cdracars@gmail.com>
2014-10-21 10:44:57 -0500

    Lets make some rsa work

---
+++ travis_before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -v
+ 
+# Fetch the CI GitHub user's encrypted private key
+curl -L -o travisci_rsa.enc "$CI_GITHUB_USER_PRIVATE_KEY_URL"
+ 
+# Decrypt the CI GitHub user's private key and add it to SSH key list
+openssl aes-256-cbc -k "$CI_GITHUB_USER_PRIVATE_KEY_SECRET" -in travisci_rsa.enc -d -a -out id_rsa
+ssh-add -D
+chmod 600 id_rsa
+ssh-add ./id_rsa
+ 
+# Init all submodules now that CI GitHub user's private key is added
+git submodule update --init --recursive
