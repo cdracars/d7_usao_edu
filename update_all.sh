ccb1fd80cf33e5ff3489f43b7a97a7b652ae4915
cdracars <cdracars@gmail.com>
2015-02-17 15:54:52 -0600

    make all call modules for DRY

--- update_all.sh
+++ update_all.sh
@@ -4,7 +4,5 @@ cd ../../;
 pwd
 drush make profiles/d7_usao_edu/d7_usao_edu.core -y;
 drush updb -y;
-cd profiles/d7_usao_edu
-pwd
-drush make --no-core --contrib-destination=. d7_usao_edu.make -y;
-drush updb -y;
+
+bash update_modules.sh
