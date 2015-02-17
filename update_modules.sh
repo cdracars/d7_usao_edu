8d5b1a50b85106397133b21cc7d14d52c0172c67
cdracars <cdracars@gmail.com>
2015-02-17 15:53:54 -0600

    adding rename to the module update

--- update_modules.sh
+++ update_modules.sh
@@ -3,3 +3,6 @@
 pwd
 drush make --no-core --contrib-destination=. d7_usao_edu.make -y;
 drush updb -y;
+
+echo "Renaming Icomoon css file."
+cp libraries/mmenu/icomoon/styles.css libraries/mmenu/icomoon/icomoon.css
