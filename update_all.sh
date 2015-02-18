97ed4f0d5ae816c4c5e88a481e57ed7aec1b0671
Cody Dracars <cdracars@gmail.com>
2015-02-18 10:05:10 -0600

    updates for scripts to work with alias

--- update_all.sh
+++ update_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cd ../../;
-pwd
-drush make profiles/d7_usao_edu/d7_usao_edu.core -y;
-drush updb -y;
-
-bash update_modules.sh
+alias=$1
+if [[ -z "$alias" ]]; then
+    echo "Drush Alias Required."
+else
+    cd ../../;
+    pwd
+    drush $alias make profiles/d7_usao_edu/d7_usao_edu.core -y;
+    drush $alias updb -y;
+    cd profiles/d7_usao_edu
+    pathToShell= pwd   
+    chmod a+x $pathToShell"update_modules.sh"
+    ./$pathToShell"update_modules.sh" $alias
+fi
--- update_modules.sh
+++ update_modules.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-pwd
-drush make --no-core --contrib-destination=. d7_usao_edu.make -y;
-drush updb -y;
-
-echo "Renaming Icomoon css file."
-cp libraries/mmenu/icomoon/styles.css libraries/mmenu/icomoon/icomoon.css
+alias=$1
+if [[ -z "$alias" ]]; then
+    echo "Drush Alias Required"
+else
+    pwd
+    drush $alias make --no-core --contrib-destination=. d7_usao_edu.make -y;
+    drush $alias updb -y;
+    echo "Renaming Icomoon css file."
+    cp libraries/mmenu/icomoon/style.css libraries/mmenu/icomoon/icomoon.css
+fi
